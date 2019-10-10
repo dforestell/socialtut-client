455dadf0d68273098c4cbf80e64b3c9a99498ad7
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-09 19:07:32 -0500

    add signup functionallity that notifies the user of empty fields, mismatched passwords, weak passwords and emails or handles that are already taken, if none of the above are hit then a user is a created and a moved to the home page while we save the auth token to local storage

--- src/pages/login.js
+++ src/pages/login.js
@@ -10,7 +10,6 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
-import { relative } from "path";
 
 const styles = {
   form: {
@@ -60,6 +59,7 @@ class login extends Component {
     axios
       .post("/login", userData)
       .then(res => {
+        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
         this.setState({ loading: false });
         this.props.history.push("/");
       })
