3258c6d817788ea634b5427a4785505d88af21a3
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-10-24 21:39:18 -0500

    rewrite authroute to utilize redux, not allowing logged in users to visit the login or signup route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import jwtDecode from "jwt-decode";
 //redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
+
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 //components
@@ -16,6 +19,7 @@ import AuthRoute from "./util/AuthRoute";
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import axios from "axios";
 
 const theme = createMuiTheme({
   palette: {
@@ -34,18 +38,19 @@ const theme = createMuiTheme({
   }
 });
 
-let authenticated = false;
 const token = localStorage.FBIdToken;
 
-// if (token) {
-//   const decodedToken = jwtDecode(token);
-//   if (decodedToken.exp * 1000 < Date.now()) {
-//     window.location.href = "/login";
-//     authenticated = true;
-//   } else {
-//     authenticated = true;
-//   }
-// }
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    store.dispatch(getUserData());
+  }
+}
 
 function App() {
   return (
@@ -56,18 +61,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={home} />
-              <AuthRoute
-                exact
-                path="/login"
-                component={login}
-                authenticated={authenticated}
-              />
-              <AuthRoute
-                exact
-                path="/signup"
-                component={signup}
-                authenticated={authenticated}
-              />
+              <AuthRoute exact path="/login" component={login} />
+              <AuthRoute exact path="/signup" component={signup} />
             </Switch>
           </div>
         </Router>
