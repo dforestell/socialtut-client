52d4c34a785df72981bfd27447fbdf8d7c7dae7f
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-11-01 21:11:57 -0500

    set up user profile data on home page to display next to screams

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React, { Component, Profiler } from "react";
 import axios from "axios";
 import Grid from "@material-ui/core/Grid";
 
 import Scream from "../components/Scream";
+import Profile from "../components/Profile";
 
 class home extends Component {
   state = {
@@ -34,7 +35,7 @@ class home extends Component {
           {recentScreamsMarkup}
         </Grid>
         <Grid item sm={4} xs={12}>
-          <p>profile...</p>
+          <Profile />
         </Grid>
       </Grid>
     );
